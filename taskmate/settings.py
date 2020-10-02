f6f1d26b1af9fd82c2eb42b07ba3251454e25fbe
Vansham Aggarwal <vanshamagg@gmail.com>
2020-10-02 18:10:47 +0530

    Changes to Requirements.txt and Settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,4 @@ LOGIN_REDIRECT_URL = 'todolist'
 
 LOGIN_URL = 'login'
 
+django_heroku.settings(locals())
